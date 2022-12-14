00ee2cfcc0e54fec3e302ac0574450443eeeb6c2
ezra-web <349578864@gapps.yrdsb.ca>
2022-12-14 13:17:49 -0500

    Deleted comment

--- MyWorld.java
+++ MyWorld.java
@@ -1,12 +1,7 @@
 
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class MyWorld extends World
 {
 
