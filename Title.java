e8da697dd7e3060b2d617c7b02c6576feebe90a2
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2022-12-14 12:31:46 -0500

    Updated comment in titlepage

--- Title.java
+++ Title.java
@@ -1,10 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Title here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * The Title page
  */
 public class Title extends World
 {
